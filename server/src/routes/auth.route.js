9903360a51740073e521061cfef49851e26cb42f
Phyo Than Htike <phyothanhtike.ethan@gmail.com>
2025-08-24 21:45:56 +0700

    add middleware

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { login, logOut, signUp } from "../controllers/auth.controller.js";
+import { checkUser, login, logOut, signUp } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
 router.post('/logout', logOut);
+router.get('/checkUser', protectRoute, checkUser);
 
 export default router;
